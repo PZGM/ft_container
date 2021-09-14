cba162ba9050f88dd7673d6cc16befe2af538c8f
Adrien FREIRE ELEUTERIO <afreire-@e2r2p5.42.fr>
2021-09-14 18:23:26 +0200

    on a touche a stack un peu mais erreur chelou

--- test/tester18.cpp
+++ test/tester18.cpp
@@ -20,13 +20,6 @@ int main()
 		*it = i;
 		i++;
 	}
-	deque<int> dek(10, 42);
-	i = 10;
-	for (deque<int>::iterator it = dek.begin(); it != dek.end(); it++)
-	{
-		*it = i;
-		i++;
-	}
 	
 	std::cout << "===default constructor with default underlying container===" << std::endl;
 	stack<int> stk;
@@ -42,29 +35,8 @@ int main()
 	std::cout << "size = " << stk.size() << " _ empty = " << stk.empty() << " _ top = " << stk.top() << std::endl;
 
 
-	std::cout << "===copy constructor with default underlying container===" << std::endl;
-	stack<int> stk2(dek);
-
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-	
-	stk2.push(42);;
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-
-	stk2.push(21);
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-
-	stk2.pop();
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-
-	stk2.pop();
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-
-	stk2.pop();
-	std::cout << "size = " << stk2.size() << " _ empty = " << stk2.empty() << " _ top = " << stk2.top() << std::endl;
-
-
 	std::cout << "===copy constructor with list as underlying container===" << std::endl;
-	stack<int, list<int>> stk3(lst);
+	stack<int, list<int> > stk3(lst);
 
 	std::cout << "size = " << stk3.size() << " _ empty = " << stk3.empty() << " _ top = " << stk3.top() << std::endl;
 	
